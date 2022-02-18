c06c96bf1454022dc1d48077574c9ffa410794ef
Tim Phillips <timdphillips@gmail.com>
2022-02-18 13:46:18 +0000

    Add more images to test lazy loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,59 @@ import Title from '@/components/Title';
 
 export default function Home() {
   return (
-    <main>
+    <main style={{ display: 'flex', flexDirection: 'column', gap: 20 }}>
       <Title>Hello TypeScript!</Title>
       <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-      <StaticImage
-        src="../images/IMG_8551.jpeg"
-        alt="Ibiza"
-        placeholder="blurred"
-        quality={35}
-      />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <StaticImage
+          src="../images/IMG_7688.jpeg"
+          alt="Ibiza"
+          placeholder="blurred"
+          quality={35}
+          width={800}
+        />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <StaticImage
+          src="../images/IMG_7696.jpeg"
+          alt="Ibiza"
+          placeholder="blurred"
+          quality={35}
+          width={800}
+        />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <StaticImage
+          src="../images/IMG_7809.jpeg"
+          alt="Ibiza"
+          placeholder="blurred"
+          quality={35}
+          width={800}
+        />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <StaticImage
+          src="../images/IMG_7898.jpeg"
+          alt="Ibiza"
+          placeholder="blurred"
+          quality={35}
+          width={800}
+        />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <StaticImage
+          src="../images/IMG_8551.jpeg"
+          alt="Ibiza"
+          placeholder="blurred"
+          quality={35}
+          width={800}
+        />
+      </div>
     </main>
   );
 }
